df97ac9bda57c2e919f3cd1530ae7cb3de4bf64d
sankar0812 <134912048+sankar0812@users.noreply.github.com>
2023-09-25 12:22:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ app=Flask(__name__)
 app.config["MYSQL_HOST"]="localhost"
 app.config["MYSQL_USER"]="root"
 app.config["MYSQL_PASSWORD"]=""
+app.config["MYSQL_PORT"]= 3306
 app.config["MYSQL_DB"]="new_db1"
 app.config["MYSQL_CURSORCLASS"]="DictCursor"
 mysql=MySQL(app)
